33aea19338b56b12d98ac49bc5f899c807a27fcd
Archi <129053616+OG-UZOK@users.noreply.github.com>
2024-06-27 12:43:25 +0700

    add null params

--- src/main/java/com/example/menu_planner/model/dtoInput/IngridientInDishRequest.java
+++ src/main/java/com/example/menu_planner/model/dtoInput/IngridientInDishRequest.java
@@ -8,10 +8,10 @@ public record IngridientInDishRequest(
 
         @NotNull(message = "Ingridient ID cannot be null")
         UUID ingridient_id,
-        @NotNull(message = "Amount cannot be null")
+
         @Positive(message = "Amount must be greater than zero")
         Double amount,
-        @NotBlank(message = "Unit cannot be empty")
+
         String unit,
         @NotNull(message = "gramm cannot be null")
         @PositiveOrZero(message = "gramm must be greater than or equal to zero")
